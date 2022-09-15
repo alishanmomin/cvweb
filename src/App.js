8f8e4f020e67cdc514edea6ae3ba4bc6d5ea9d29
alishanmomin <mominalishan@gmail.com>
2022-09-15 13:56:33 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/app.scss'
 import "./assets/fontawesome/css/all.css";
 import Home from './pages/home';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App()
 {
     return (
-        <div>
-            <h2>Hello</h2>
-            <MessengerCustomerChat
-                pageId="106249202238135"
-                appId="604291807829619"
-            />,
-        </div>
-        // <Home />
+        <Home />
     );
 }
 
